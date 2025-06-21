238041ea0ae34ad2133c8afde7f695b43eabe088
Korolenko <korolenkoalexey84@gmail.com>
2025-06-21 21:38:30 +0300

    second commit

--- src/task-6.ts
+++ src/task-6.ts
@@ -5,3 +5,9 @@ function getFirstElement<T>(arr: T[]): T {
 getFirstElement<number>([1, 2, 3]);         
 getFirstElement<string>(["a", "b", "c"]);
 getFirstElement<boolean>([true, false, true]);
+
+
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([true, false, true]));
+
